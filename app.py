dec6da59c9d4b8821f6a7e5b48a35c225751d7da
Tony Assi <tonyassi@users.noreply.huggingface.co>
2024-01-03 23:48:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import gradio as gr
 from transformers import pipeline
 from PIL import Image, ImageOps
+import time
 
 # Initialize Segmentation Pipeline
 segformer_b2_clothes_pipe = pipeline("image-segmentation", model="mattmdjaga/segformer_b2_clothes")
@@ -13,11 +14,15 @@ def segformer_b2_clothes(img):
     return img
 
 def remove_background(img):
+    start = time.time()
     segformer_b2_clothes_result = segformer_b2_clothes(img)
+    end = time.time()
+    segformer_b2_clothes_text = "[mattmdjaga/segformer_b2_clothes](https://huggingface.co/mattmdjaga/segformer_b2_clothes) in " + str(end-start) + " seconds"
     
-    return segformer_b2_clothes_result
+    return segformer_b2_clothes_text, segformer_b2_clothes_result
 
 iface = gr.Interface(fn=remove_background, 
                      inputs=gr.Image(type='pil'), 
-                     outputs=gr.Image(label='segformer_b2_clothes', type='pil'))
+                     outputs=[gr.Markdown(),
+                              gr.Image(label='segformer_b2_clothes', type='pil')])
 iface.launch()
