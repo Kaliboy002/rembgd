97c5c4851aeb7e7db45bdc3a5846c93ccb80aff5
Tony Assi <tonyassi@users.noreply.huggingface.co>
2024-01-03 23:36:38 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import gradio as gr
+from transformers import pipeline
+from PIL import Image, ImageOps
+
+# Initialize Segmentation Pipeline
+segformer_b2_clothes_pipe = pipeline("image-segmentation", model="mattmdjaga/segformer_b2_clothes")
+
+def segformer_b2_clothes(img):
+    result = segmentation_pipe(img)
+    mask = result[0]['mask'].convert('L')
+    mask = ImageOps.invert(mask)
+    img.putalpha(mask)
+    return img
+
+def remove_background(img):
+    segformer_b2_clothes_result = segformer_b2_clothes(img)
+
+	return segformer_b2_clothes_result
+
+iface = gr.Interface(fn=remove_background, 
+                     inputs=gr.Image(type='pil'), 
+                     outputs=gr.Image(label='segformer_b2_clothes', type='pil'))
+iface.launch()
